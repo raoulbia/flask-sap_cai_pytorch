05cdef6585c80fcd1e86d1015138cf2ec619b3ab
vagrant <raoulbia@gmail.com>
2019-07-19 21:55:45 +0100

    update

--- cai_pytorch/make_dataset.py
+++ cai_pytorch/make_dataset.py
@@ -22,7 +22,7 @@ def squad():
     # print(len(df))
     # pprint(df.context.iloc[0])
     # print(df.tail())
-    pairs = list(zip(df['question'].tolist(), df['context'].tolist()))
+    pairs = list(zip(df['question'].tolist(), df['text'].tolist()))
     # print(pairs)
     return pairs
 
